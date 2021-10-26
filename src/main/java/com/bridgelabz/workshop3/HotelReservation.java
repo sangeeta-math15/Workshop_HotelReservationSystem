549ce4bc392822794b85e27a2b1f42723ddfa8e8
sngtmth <sngtmth@gmail.com>
2021-10-26 08:48:15 +0530

    Added:Welcome Message

---
+++ src/main/java/com/bridgelabz/workshop3/HotelReservation.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.workshop3;
+
+public class HotelReservation {
+    //DriverCode
+    public static void main(String[] args) {
+        //Print Welcome Message
+        System.out.println("Welcome to Hotel Reservation System");
+    }
+}
